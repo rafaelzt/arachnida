f7d16fc88baab50b9136f64f54b31dbb7edc1084
Rafael Z Tanganelli <kabelu@gmail.com>
2023-05-22 18:20:04 +0200

    update scorpion

--- scorpion.py
+++ scorpion.py
@@ -6,51 +6,55 @@
 #    By: rzamolo- <rzamolo-@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/05/20 15:56:50 by rzamolo-          #+#    #+#              #
-#    Updated: 2023/05/22 13:49:15 by rzamolo-         ###   ########.fr        #
+#    Updated: 2023/05/22 18:13:43 by rzamolo-         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
 import sys
-import exiftool
+from exiftool import ExifTool
 import argparse
+import glob
 
 def read_metadata(file):
-    with exiftool.ExifToolHelper() as file_data:
-        metadata = file_data.get_metadata(file)
+    with ExifTool() as file_data:
+        metadata = file_data.execute_json('-G', '-j', file)
         for item in metadata:
             try:
                 for tag, value in item.items():
-                    print("{:<40}:\t{:<30}".format(tag, value))
+                    if isinstance(value, list):
+                        value_str = ", ".join(str(v) for v in value)
+                        print("{:<40}:\t{:<30}".format(tag, value_str))
+                    else:
+                        print("{:<40}:\t{:<30}".format(tag, value))
             except:
                 print(">> ERROR: Reading file tag")
 
-def remove_metadata(file, tags):
+def remove_metadata(file, tags="all"):
     with ExifTool() as file_data:
-        file_data.delete_tags(file, tags)
-
+        if tags == "all":
+            file_data.execute("-all=", file)
+            print(">> Removed all tags")
+        else:
+            for tag in tags:
+                file_data.execute("-{}=".format(tag), file)
+                print(">> Removed tag", tag)
 
 if __name__ == "__main__":
-    
-    for item in sys.argv[1:]:
-        parser = argparse.ArgumentParser(description='Read or remove metadata from image')
+    parser = argparse.ArgumentParser(description='Read or remove metadata from an image')
+    parser.add_argument('image', nargs='+', help='image names or wildcard patterns')
+    parser.add_argument('-r', '--remove', dest="remove", action='store_true', help='Remove metadata')
+    parser.add_argument('-t', '--tags', metavar="[TAGS]", dest='tags', type=str, default="all", help='Tags to remove (default all)')
+    args = parser.parse_args()
 
-        parser.add_argument('image',
-                            help='image name')
-        parser.add_argument('-r', '--remove',
-                            dest="remove",
-                            action='store_true',
-                            help='Remove metadata')
-        parser.add_argument('-t', '--tags',
-                            metavar="[TAGS]",
-                            dest='tags',
-                            type=str,
-                            default="all",
-                            help='Tags to remove (default all)')
-        args = parser.parse_args()
-        if args.remove:
-            print("-> Removing metadata from", item)
-            remove_metadata(item, args.tags)
-        else:        
-            print("-> Reading metadata from", item)
-            read_metadata(item)
-        
+    image_files = []
+    for pattern in args.image:
+        image_files.extend(glob.glob(pattern))
+
+    if args.remove:
+        for image_file in image_files:
+            print("-> Removing metadata from", image_file)
+            remove_metadata(image_file, args.tags)
+    else:
+        for image_file in image_files:
+            print("-> Reading metadata from", image_file)
+            read_metadata(image_file)
